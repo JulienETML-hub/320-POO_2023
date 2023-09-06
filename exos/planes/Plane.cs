62c641d8b8a4da512694f03b00fb367e80efcc9c
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2023-09-06 11:20:25 +0200

    fin de cours

--- exos/planes/Plane.cs
+++ exos/planes/Plane.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace planes
 {
-    internal class Plane
+    class Plane
     {
+        
         private string[] view =
-    {
+        {
             @" _                         ",
             @"| \                        ",
             @"|  \       ______          ",
@@ -17,6 +18,29 @@ namespace planes
             @"  \_______ --------- __>-} ",
             @"        \_____|_____/   |  "
         };
+        public void show()
+        {
+            
+                for (int i = 0; i < view.Length; i++)
+                {
+                    
+                    Console.WriteLine(view[i]);
+                }
+
+
+        }
+         public void move(int _x, int _y) 
+         {
+            /*int xAvion = _x;
+            int yAvion = _y;
+            xAvion++;*/
+            Console.SetCursorPosition(_x, _y);
+            _x++;
+            _y++;
+             Thread.Sleep(50);
+             Console.Clear();
+
+         }
 
     }
 }
--- exos/planes/Program.cs
+++ exos/planes/Program.cs
@@ -1,2 +1,15 @@
 ﻿using planes;
 
+
+
+
+
+Plane avion1 = new Plane();
+for (int o = 0; o < 60; o++) 
+{
+    (int Left, int Top) cursorPosition = Console.GetCursorPosition();
+
+    avion1.move(cursorPosition.Left, cursorPosition.Top);
+    avion1.show();
+}
+Console.Read();
