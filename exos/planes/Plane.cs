31a8a5b455fb2160f2a8d83095e10cb424b26795
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2023-09-06 08:52:44 +0200

    feat(classPlane)
    Création class plane

---
+++ exos/planes/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace planes
+{
+    internal class Plane
+    {
+        private string[] view =
+    {
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
+
+    }
+}
